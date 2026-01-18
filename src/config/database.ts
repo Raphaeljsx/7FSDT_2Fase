f866de85818dbf7becbb9035efa106ab19a42ed6
RaphaelJsx <raphaelalvoliv@gmail.com>
2026-01-17 22:18:03 -0300

    add: adicionei o arquivo de logger

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,12 +1,12 @@
 import { Pool } from "pg";
 
-//configura a conexão com o banco
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
